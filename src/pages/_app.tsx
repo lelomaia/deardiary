5686bbc1d41a2143cb042925517791f2d8f2d330
Lelo Maia <lelo@tonarocket.com.br>
2022-01-17 20:17:46 -0300

    SignIn com Github configurado e funcionando normalmente. Verificando Si gnOut do App, não está funcionando corretamente.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import {AppProps} from 'next/app';
-
-import '../../styles/global.scss';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
 import { Header } from '../components/Header';
 
+import '../../styles/global.scss';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetStaticProps } from 'next';
+
 import Image from 'next/image';
 import Head from 'next/head';
 
-import styles from './home.modules.scss';
+import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -12,19 +23,40 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Olá, seja bem-vindo(a)</span>
-          <h1><span>Registre todos os seus momentos </span>e não perca nada</h1>
+          <h1><span>Registre todos os seus momentos </span><br />e não perca nada</h1>
           <p>
-            Tenha acesso aos recursos completos <br />
-            <span>por R$9,90/mês</span>
+            Tenha acesso a todos os recursos <br />
+            <span>por {product.amount}/mês</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image
           src="/images/ilustra-home.svg"
-          alt="Girl coding"
+          alt="Girl writing"
           width="585"
           height="484"
         />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1KIauxLRywkHGWxrM1Zzky4V')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }
